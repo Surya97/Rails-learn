a28e013b8202566dee31daabd96ab6d122435a44
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2020-05-17 12:36:48 -0700

    Modified flash messages and singup button test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = 'User signup successful.'
+      flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed up!"
       redirect_to articles_path
     else
       render 'new'
