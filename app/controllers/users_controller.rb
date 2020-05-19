81b5aadc9d56c714794d35a095bdb34a6bef8f1e
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2020-05-19 08:19:12 -0700

    Added show method to user controller and added the view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:edit, :update]
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def show
+  end
 
   def new
     @user = User.new
